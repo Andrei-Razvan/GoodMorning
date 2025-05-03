e05bb1e757ffaa1ef1305626a687000228e66c8d
Andrei <andreicamenic@yahoo.com>
2025-05-03 02:37:39 +0100

    second commit

--- src/main/java/uk/lset/controller/Controller.java
+++ src/main/java/uk/lset/controller/Controller.java
@@ -1,20 +1,28 @@
 package uk.lset.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    @GetMapping(path = "/get")
+    public Boolean get() {
+        logger.info("Get Request");
+        return true;
+    }
 
     @PostMapping(path = "/post")
     public String post() {
         return "Good Morning";
     }
 
-    @GetMapping(path = "/get")
-    public Boolean get() {
-        return true;
-    }
 
 }
